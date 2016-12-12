577bea69efe9549611aa1261dd9f8bc734bfb2ba
Enda Lee <enda.lee@ittdublin.ie>
2016-12-12 18:53:21 +0000

    Upload CRUD part 1 not working

---
+++ app/models/Product.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.data.format.*;
+import play.data.validation.*;
+
+import com.avaje.ebean.*;
+
+
+@Entity
+public class Product extends Model {
+
+@Id
+private Long id;
+
+@Constraints.Required
+private String Name;
+
+@Constraints.Required
+private String Discription;
+
+@Constraints.Required
+private int stock;
+
+@Constraints.Required
+private double price;
+
+public Product() {
+}
+
+public Product (Long id, String name, String description, int stock, double price) {
+
+  this.id = id;
+  this.name = name;
+  this.description = description;
+  this.stock = stock;
+  this.price = price;
+}
+
+public static Finder<Long,Product> find = new Finder<Long,Product>(Product.class);
+
+public static List<Product> findAll(){
+    return Product.find.all();
+}
+
+public Long getId(){
+    return id;
+}
+
+public void setId(Long id){
+      this.id = id;
+}
+
+public String getName(){
+    return name;
+}
+
+} 
